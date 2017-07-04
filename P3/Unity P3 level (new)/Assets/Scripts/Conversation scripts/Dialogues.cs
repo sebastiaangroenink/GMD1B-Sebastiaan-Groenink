7c72b936755d96cc1e62dd9990396c201a052da3
sebastiaangroeninkDeltion <sebastiaangroenink@gmail.com>
2017-07-04 19:53:16 +0200

    re-upload van scene P3

---
+++ P3/Unity P3 level (new)/Assets/Scripts/Conversation scripts/Dialogues.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogues : MonoBehaviour {
+    public List<string> dialogueList = new List<string>();
+    public List<string> questionList = new List<string>();
+    public int currDial;
+    public int addPos;
+    public int addNeg;
+    public bool firstReply;
+  
+
+    public void Chat()  //asked when yes/no button is pressed.
+    {
+        if (firstReply && currDial % 2 == 0) // done first time replying to set addPos and NegPos correctly in case a player has to answer a question at an odd index instead of an even index.
+        {
+            addPos = 2;
+            addNeg = 1;
+            firstReply = false;
+            Chat();
+        }
+        else if(firstReply && currDial %2 !=0)
+        {
+            addPos = 1;
+            addNeg = 2;
+            firstReply = false;
+            Chat();
+        }
+        else if (!firstReply) { 
+
+        if (currDial%2 == 0)
+        {
+            //is even. Negative response
+            currDial += addNeg;
+        }
+            if (currDial % 2 != 0)
+            {
+                //is odd. Positive response
+                currDial += addPos;
+            }
+             
+        }
+    }
+}
---
+++ P3/Unity P3 level (new)/Assets/[Audio]/Scripts/Spectrum.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spectrum : MonoBehaviour 
+{
+	public GameObject prefab;
+	public int objectCount = 24;
+	public float radius = 4;
+	public GameObject[] cubes;
+
+	// Use this for initialization
+	void Start () 
+	{
+		for(int i = 0; i < objectCount; i++)
+		{
+			float angle = i * Mathf.PI * 2 / objectCount;
+			Vector3 pos = new Vector3(Mathf.Sin(angle), 0, Mathf.Cos(angle)) * radius;
+			GameObject cube = Instantiate(prefab, pos, Quaternion.identity) as GameObject;
+			cube.transform.parent = this.transform;
+			cube.name = "SoundCube"+(i+1);
+		}
+
+		cubes = GameObject.FindGameObjectsWithTag("Sound Cube");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float[] spectrum = new float[64];
+		AudioListener.GetOutputData(spectrum, 0);
+
+		for (int i = 0; i < cubes.Length; i++)
+		{
+			Vector3 previousScale = cubes[i].transform.localScale;
+			previousScale.y = Mathf.Lerp (previousScale.y, spectrum[i] * 50, Time.deltaTime * 10 );
+
+			//Vector3 previousScale = Vector3.one;
+
+			cubes[i].transform.localScale = previousScale;
+		}
+	}
+}
